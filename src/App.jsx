a024eab0089902b8af9756f794817a7c9d8f7a28
Youssef Ben Omrane <125128369+Youuusseff@users.noreply.github.com>
2024-03-09 15:50:38 +0000

    Update personal information and images

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Youssef Benomrane",
+  title: "Web Developer & Data Analyst",
+  email: "youssefbenomrane45@gmail.com",
+  gitHub: "Youuusseff",
+  instagram: "youssef_benomrane",
+  linkedIn: "youssef-benomrane",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
